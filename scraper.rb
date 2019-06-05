a5c5fa467fd5e6930fba530f7f3cc11635284fbc
Matthew Landauer <mlandauer@gmail.com>
2019-06-05 15:11:50 +1000

    If one of the authorities fail don't bring down other authorities

--- scraper.rb
+++ scraper.rb
@@ -1,14 +1,22 @@
 require 'epathway_scraper'
 
-# TODO: If scraping on one authority fails then don't stop for other authorities
-# but still raise an error at the end
-
+exceptions = []
 EpathwayScraper::AUTHORITIES.keys.each do |authority_label|
   puts "\nScraping authority #{authority_label}..."
-  EpathwayScraper.scrape_authority(authority_label) do |record|
-    record["authority_label"] = authority_label.to_s
+  begin
+    EpathwayScraper.scrape_authority(authority_label) do |record|
+      record["authority_label"] = authority_label.to_s
 
-    EpathwayScraper.log(record)
-    ScraperWiki.save_sqlite(["authority_label", "council_reference"], record)
+      EpathwayScraper.log(record)
+      ScraperWiki.save_sqlite(["authority_label", "council_reference"], record)
+    end
+  rescue StandardError => e
+    STDERR.puts "#{authority_label}: ERROR: #{e}"
+    STDERR.puts e.backtrace
+    exceptions << e
   end
+end
+
+unless exceptions.empty?
+  raise "There were earlier errors. See output for details"
 end
