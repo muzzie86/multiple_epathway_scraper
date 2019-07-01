dbc437af0751682fca539bfcbdfbb3578347789e
Matthew Landauer <mlandauer@gmail.com>
2019-07-01 16:10:45 +1000

    Automatically retry failed authorities at the end.

--- scraper.rb
+++ scraper.rb
@@ -1,22 +1,37 @@
 require 'epathway_scraper'
 
-exceptions = []
-EpathwayScraper::AUTHORITIES.keys.each do |authority_label|
-  puts "\nScraping authority #{authority_label}..."
-  begin
-    EpathwayScraper.scrape_authority(authority_label) do |record|
-      record["authority_label"] = authority_label.to_s
+def scrape(authorities)
+  exceptions = {}
+  authorities.each do |authority_label|
+    puts "\nScraping authority #{authority_label}..."
+    begin
+      EpathwayScraper.scrape_authority(authority_label) do |record|
+        record["authority_label"] = authority_label.to_s
 
-      EpathwayScraper.log(record)
-      ScraperWiki.save_sqlite(["authority_label", "council_reference"], record)
+        EpathwayScraper.log(record)
+        ScraperWiki.save_sqlite(["authority_label", "council_reference"], record)
+      end
+    rescue StandardError => e
+      STDERR.puts "#{authority_label}: ERROR: #{e}"
+      STDERR.puts e.backtrace
+      exceptions[authority_label] = e
     end
-  rescue StandardError => e
-    STDERR.puts "#{authority_label}: ERROR: #{e}"
-    STDERR.puts e.backtrace
-    exceptions << e
   end
+  exceptions
+end
+
+authorities = EpathwayScraper::AUTHORITIES.keys
+puts "Scraping authorities: #{authorities.join(', ')}"
+exceptions = scrape(authorities)
+
+unless exceptions.empty?
+  puts "\n***************************************************"
+  puts "Now retrying authorities which earlier had failures"
+  puts "***************************************************"
+
+  exceptions = scrape(exceptions.keys)
 end
 
 unless exceptions.empty?
-  raise "There were earlier errors. See output for details"
+  raise "There were errors with the following authorities: #{exceptions.keys}. See earlier output for details"
 end
