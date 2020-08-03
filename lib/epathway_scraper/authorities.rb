8234f723ac154562a15d81e60368394711eab738
Matthew Landauer <mlandauer@gmail.com>
2020-08-03 14:29:55 +1000

    Sort authorities into alphabetical order

--- lib/epathway_scraper/authorities.rb
+++ lib/epathway_scraper/authorities.rb
@@ -3,70 +3,6 @@
 module EpathwayScraper
   # The settings to scrape each different authority
   AUTHORITIES = {
-    greater_shepparton: {
-      url: "https://eservices.greatershepparton.com.au/ePathway/Production",
-      state: "VIC",
-      list: :last_30_days,
-      # Has an incomplete SSL chain: See
-      # https://www.ssllabs.com/ssltest/analyze.html?d=eservices.greatershepparton.com.au&latest
-      disable_ssl_certificate_check: true
-    },
-    latrobe: {
-      url: "https://eservices.latrobe.vic.gov.au/ePathway/Production",
-      state: "VIC",
-      list: :last_30_days
-    },
-    whitehorse: {
-      url: "https://eservices.whitehorse.vic.gov.au/ePathway/Production",
-      state: "VIC",
-      list: :last_30_days
-    },
-    port_phillip: {
-      url: "https://eservices.portphillip.vic.gov.au/ePathway/Production",
-      state: "VIC",
-      list: :last_30_days
-    },
-    mooney_valley: {
-      url: "https://online.mvcc.vic.gov.au/ePathway/Production",
-      state: "VIC",
-      list: :advertising
-    },
-    macedon_ranges: {
-      url: "https://eservices.mrsc.vic.gov.au/ePathway/Production",
-      state: "VIC",
-      list: :last_30_days
-    },
-    maroondah: {
-      url: "https://eservices.maroondah.vic.gov.au/ePathway/Production",
-      state: "VIC",
-      list: :advertising
-    },
-    frankston: {
-      url: "https://epathway.frankston.vic.gov.au/ePathway/Production",
-      state: "VIC",
-      list: :last_30_days
-    },
-    east_gippsland: {
-      url: "https://epathway.egipps.vic.gov.au/ePathway/Production",
-      state: "VIC",
-      list: :last_30_days
-    },
-    bendigo: {
-      url: "https://epathway.bendigo.vic.gov.au/ePathway/Production",
-      state: "VIC",
-      # Not using :last_30_days because the bendigo server has a problem
-      list: :advertising
-    },
-    rockhampton: {
-      url: "https://eservices.rockhamptonregion.qld.gov.au/ePathway/ePathProd",
-      state: "QLD",
-      list: :last_30_days
-    },
-    livingstone: {
-      url: "http://mpathway.livingstone.qld.gov.au/ePathway/Production",
-      state: "QLD",
-      list: :last_30_days
-    },
     adelaide: {
       url: "https://epathway.adelaidecitycouncil.com/epathway/ePathwayProd",
       state: "SA",
@@ -83,6 +19,12 @@ module EpathwayScraper
       list: :last_30_days,
       force_detail: true
     },
+    bendigo: {
+      url: "https://epathway.bendigo.vic.gov.au/ePathway/Production",
+      state: "VIC",
+      # Not using :last_30_days because the bendigo server has a problem
+      list: :advertising
+    },
     campbelltown: {
       url: "https://ebiz.campbelltown.nsw.gov.au/ePathway/Production",
       state: "NSW",
@@ -92,6 +34,21 @@ module EpathwayScraper
       url: "https://eservices.darebin.vic.gov.au/ePathway/Production",
       state: "VIC",
       list: :all_this_year
+    },
+    east_gippsland: {
+      url: "https://epathway.egipps.vic.gov.au/ePathway/Production",
+      state: "VIC",
+      list: :last_30_days
+    },
+    frankston: {
+      url: "https://epathway.frankston.vic.gov.au/ePathway/Production",
+      state: "VIC",
+      list: :last_30_days
+    },
+    gladstone: {
+      url: "https://online.gladstone.qld.gov.au/ePathway/eProd",
+      state: "QLD",
+      list: :last_30_days
     },
     glen_eira: {
       url: "https://epathway-web.gleneira.vic.gov.au/ePathway/Production",
@@ -103,6 +60,14 @@ module EpathwayScraper
       url: "https://cogc.cloud.infor.com/ePathway/epthprod",
       state: "QLD",
       list: :last_30_days
+    },
+    greater_shepparton: {
+      url: "https://eservices.greatershepparton.com.au/ePathway/Production",
+      state: "VIC",
+      list: :last_30_days,
+      # Has an incomplete SSL chain: See
+      # https://www.ssllabs.com/ssltest/analyze.html?d=eservices.greatershepparton.com.au&latest
+      disable_ssl_certificate_check: true
     },
     greatlakes: {
       url: "https://services.greatlakes.nsw.gov.au/ePathway/Production",
@@ -126,8 +91,38 @@ module EpathwayScraper
       state: "VIC",
       list: :advertising
     },
+    lake_macquarie: {
+      url: "https://property.lakemac.com.au/ePathway/Production",
+      state: "NSW",
+      list: :last_30_days
+    },
+    latrobe: {
+      url: "https://eservices.latrobe.vic.gov.au/ePathway/Production",
+      state: "VIC",
+      list: :last_30_days
+    },
+    livingstone: {
+      url: "http://mpathway.livingstone.qld.gov.au/ePathway/Production",
+      state: "QLD",
+      list: :last_30_days
+    },
+    macedon_ranges: {
+      url: "https://eservices.mrsc.vic.gov.au/ePathway/Production",
+      state: "VIC",
+      list: :last_30_days
+    },
+    maroondah: {
+      url: "https://eservices.maroondah.vic.gov.au/ePathway/Production",
+      state: "VIC",
+      list: :advertising
+    },
     monash: {
       url: "https://epathway.monash.vic.gov.au/ePathway/Production",
+      state: "VIC",
+      list: :advertising
+    },
+    mooney_valley: {
+      url: "https://online.mvcc.vic.gov.au/ePathway/Production",
       state: "VIC",
       list: :advertising
     },
@@ -145,6 +140,16 @@ module EpathwayScraper
       url: "http://pathway.onkaparinga.sa.gov.au/ePathway/Production",
       state: "SA",
       list: :all_this_year
+    },
+    port_phillip: {
+      url: "https://eservices.portphillip.vic.gov.au/ePathway/Production",
+      state: "VIC",
+      list: :last_30_days
+    },
+    rockhampton: {
+      url: "https://eservices.rockhamptonregion.qld.gov.au/ePathway/ePathProd",
+      state: "QLD",
+      list: :last_30_days
     },
     salisbury: {
       url: "https://eservices.salisbury.sa.gov.au/ePathway/Production",
@@ -174,6 +179,11 @@ module EpathwayScraper
       # https://www.ssllabs.com/ssltest/analyze.html?d=epathway.wtcc.sa.gov.au
       disable_ssl_certificate_check: true
     },
+    whitehorse: {
+      url: "https://eservices.whitehorse.vic.gov.au/ePathway/Production",
+      state: "VIC",
+      list: :last_30_days
+    },
     wollongong: {
       url: "http://epathway.wollongong.nsw.gov.au/ePathway/Production",
       state: "NSW",
@@ -184,16 +194,6 @@ module EpathwayScraper
       state: "VIC",
       list: :all,
       max_pages: 20
-    },
-    lake_macquarie: {
-      url: "https://property.lakemac.com.au/ePathway/Production",
-      state: "NSW",
-      list: :last_30_days
-    },
-    gladstone: {
-      url: "https://online.gladstone.qld.gov.au/ePathway/eProd",
-      state: "QLD",
-      list: :last_30_days
     }
   }.freeze
 end
